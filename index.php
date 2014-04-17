1e6684a1b1e6d6f2bc9843df7d9aa5cae3e97f17
tao-s <tao@xross-cube.com>
2014-04-17 17:02:52 +0900

    return var 追加

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ session_start();
 if(isset($_POST["command"])){
     $command = $_POST["command"];
     if(!empty($command) and $_SESSION["token"] == $_POST["token"]){
-        //$res = shell_exec($command);
-        passthru($command);
+        $res = shell_exec($command,$return_var);
+        //passthru($command);
     }
 }
 $token = sha1(rand()."jng42p98weoidjsalv");
@@ -21,9 +21,10 @@ $_SESSION["token"] = $token;
 <form action="?" method="post">
 <input type="hidden" value="<?php echo $token; ?>" name="token" />
 <p style="margin:0 auto;">コマンド:<input type="text" name="command" size="24" value="<?php echo htmlspecialchars($command,ENT_QUOTES); ?>" style="width:80%;" /><input type="submit" value="送信" /></p>
-<?php if(!empty($res)){ ?>
+<?php if(!empty($res) or !empty($return_var)){ ?>
 <div>
     <h2>実行結果</h2>
+    <p><?php echo $return_var; ?></p>
     <pre>
     <?php echo htmlspecialchars($res,ENT_QUOTE); ?>
     </pre>
