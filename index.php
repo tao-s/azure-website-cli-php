ffad118769a98f80e2af38a227aa7f6e435e3e5e
tao-s <tao@xross-cube.com>
2014-04-17 16:53:30 +0900

    typo修正

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ echo $command;
 <body>
 <form action="?" method="post">
 <input type="hidden" value="<?php echo $token; ?>" name="token" />
-<p style="margin:0 auto;">コマンド:<input type="text" name="command" size="24" value="<?php echo htmlspecialchars($command,ENT_QUOTE); ?>" style="width:80%;" /><input type="submit" value="送信" /></p>
+<p style="margin:0 auto;">コマンド:<input type="text" name="command" size="24" value="<?php echo htmlspecialchars($command,ENT_QUOTES); ?>" style="width:80%;" /><input type="submit" value="送信" /></p>
 <?php if(!empty($res)){ ?>
 <div>
     <h2>実行結果</h2>
