f0e38395e80541937e9e49a03c915fbdbd273408
tao-s <tao@xross-cube.com>
2014-04-17 16:36:19 +0900

    ペライチ追加

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$command = "";
+session_start();
+if(isset($_POST["command"])){
+    $command = $_POST["command"];
+    if(!empty($command) and $_SESSION["token"] == $_POST["token"]){
+        $res = shell_exec($command);
+    }
+}
+$token = sha1(rand()."jng42p98weoidjsalv");
+$_SESSION["token"] = $token;
+
+?>
+<!DOCUTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action="?" method="post">
+<input type="hidden" value="<?php echo $token; ?>" name="token" />
+<p style="margin:0 auto;">コマンド:<input type="text" name="command" size="24" value="<?php echo htmlspecialchars($command,ENT_QUOTE); ?>" style="width:80%;" /><input type="submit" value="送信" /></p>
+<?php if(!empty($res)){ ?>
+<div>
+    <h2>実行結果</h2>
+    <pre>
+    <?php echo htmlspecialchars($res,ENT_QUOTE); ?>
+    </pre>
+</div>
+<?php } ?>
+</form>
+</body>
+</html>
